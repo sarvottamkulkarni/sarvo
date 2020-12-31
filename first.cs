92001fc6370d48c489f5b89244d0a6bf3648b24a
sarvottamkulkarni <76813358+sarvottamkulkarni@users.noreply.github.com>
2020-12-31 17:57:53 +0530

    gittttt
    s

---
+++ first.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGIT
+{
+    class Github
+    {
+        public void git() 
+        {
+            Console.WriteLine("Hi GIT");
+            Console.WriteLine("Hi visual studio");
+        }
+    }
+}
